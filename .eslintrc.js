6ffc1e1062ae06f4d4918d3f482cdbb6ff388e2a
Federico Agustin Annacondia <agustinannacondia@gmail.com>
2021-10-05 19:59:29 -0300

    Config eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@ module.exports = {
     env: {
       browser: true,
       es2021: true,
+      node: true
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
+    extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
     parserOptions: {
       ecmaFeatures: {
         jsx: true,
@@ -12,12 +13,13 @@ module.exports = {
       ecmaVersion: 12,
       sourceType: "module",
     },
-    plugins: ["react"],
+    plugins: ["react", "prettier"],
     rules: {
         "no-console": "warn",
         "react/prop-types": 0,
         "react/react-in-jsx-scope": 0,
-        "linebreak-style": ["error", "unix"]
+        "linebreak-style": ["error", "unix"],
+        "no-debugger": 0
     },
     settings: {
         react: {
