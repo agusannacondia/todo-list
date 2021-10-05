5a8069188074649e7032f3e8bd6463a36df4ebf0
Federico Agustin Annacondia <agustinannacondia@gmail.com>
2021-10-05 19:59:52 -0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import pages from './pages'
 
-function App() {
-  return (
-    <p>Hola</p>
-  );
-}
+const App = () => 
+(
+    <Router>
+      <Switch>
+      {
+        pages.map((page, key) => <Route exact path={page.path} component={page.component} key={key} />)
+      }
+      </Switch>
+    </Router>
+);
 
-export default App;
+export default App
